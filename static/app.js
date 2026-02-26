8852c666da70794bbcb254d9b9bfb4b99e828682
BinDir0 <3061445744@qq.com>
2026-02-26 19:04:13 +0800

    feat: add '指代不明确' to translation error types

--- static/app.js
+++ static/app.js
@@ -1139,7 +1139,7 @@ function renderTranslationItems(translations, originalInstructions) {
         detailDiv.className = 'trans-error-detail';
         detailDiv.style.display = 'none';
         
-        const errorTypes = ['方位有问题', '物品有问题', '动作有问题', '乱码', '其他'];
+        const errorTypes = ['方位有问题', '物品有问题', '动作有问题', '指代不明确', '乱码', '其他'];
         errorTypes.forEach(errType => {
             const errLabel = document.createElement('label');
             errLabel.className = 'trans-error-type-item';
